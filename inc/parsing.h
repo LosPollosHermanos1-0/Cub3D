a8fa327cd5f6868c94c7a74bf13926690ef14498
LeondavidZipp <leondavidzipp@gmx.de>
2024-06-19 16:14:00 +0200

    updated .h file

--- inc/parsing.h
+++ inc/parsing.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 09:48:56 by lzipp             #+#    #+#             */
-/*   Updated: 2024/06/14 12:33:46 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/06/19 16:12:43 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,20 @@
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdbool.h>
+
+# include "../libft/libft.h"
+
+// map
+bool	ft_create_map(int fd, char*** map);
+
+// rgb
+bool	ft_get_rgb_arr(char **line, int ***colors_ptr);
+
+// textures
+bool	ft_get_texture(char **line, char ***textures_ptr);
+bool	ft_check_texture(char **texture);
+
+// helpers
+bool	ft_wrong_char_inside(char **str);
 
 #endif
