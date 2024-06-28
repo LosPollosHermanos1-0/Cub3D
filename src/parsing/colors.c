03ca22ce29d7bee31f228d77ac7dbb408af791d5
LeondavidZipp <leondavidzipp@gmx.de>
2024-06-28 11:54:27 +0200

    updated colors function

--- src/parsing/colors.c
+++ src/parsing/colors.c
@@ -6,17 +6,19 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 16:06:38 by lzipp             #+#    #+#             */
-/*   Updated: 2024/06/27 18:30:06 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/06/28 11:49:54 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/parsing.h"
 
-bool	ft_get_rgb_arr(char **line, int **colors_ptr)
+bool	ft_get_rgb(char **line, int ***colors_ptr)
 {
 	char	*color;
 	char	direction[2];
 	char	**rgb_str;
+	int		index;
+	int		*rgb_arr;
 	int		i;
 
 	ft_memmove(direction, *line, 1);
@@ -25,17 +27,24 @@ bool	ft_get_rgb_arr(char **line, int **colors_ptr)
 	rgb_str = ft_split(color, ',');
 	if (!rgb_str)
 		return (perror("Error: could not split color"), false);
+	rgb_arr = ft_calloc(sizeof(int), 4);
 	i = -1;
 	while (rgb_str[++i])
 	{
-		rgb_str = ft_strtrim_in_place(rgb_str, " ");
-		if (ft_isdigit(rgb_str[i]) == false)
+		if ((bool)ft_is_str_digit(rgb_str[i]) == false)
 			return (perror("Error: color must be a number"), false);
+		rgb_arr[i] = ft_atoi(rgb_str[i]);
+		if (rgb_arr[i] < 0 || rgb_arr[i] > 255)
+			return (perror("Error: rgb must be between 0 and 255"), false);
 	}
-	if (ft_strncmp)
-	{
-		
-	}
-
+	if (ft_strncmp(direction, "F", 1) == 0)
+		index = 0;
+	else if (ft_strncmp(direction, "C", 1) == 0)
+		index = 1;
+	else
+		return (perror("Error: invalid color direction"), false);
+	if (*colors_ptr[index])
+		return (perror("Error: color direction encountered twice"), false);
+	*colors_ptr[index] = rgb_arr;
 	return (true);
 }
