fdf2c9267f22f19410a87800ce5a51c0ec08d2c3
LeondavidZipp <leondavidzipp@gmx.de>
2024-07-02 13:25:20 +0200

    added get player function

--- src/parsing/validate_map.c
+++ src/parsing/validate_map.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 11:51:26 by lzipp             #+#    #+#             */
-/*   Updated: 2024/07/02 12:19:07 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/07/02 13:21:55 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,31 @@ bool	ft_check_borders(char ***map_ptr)
 	{
 		/* code */
 	}
+}
+
+/**
+ * Returns the players position.
+ *
+ * @param map_ptr Map to search player in.
+ * @return row-col position if found, else [-1, -1].
+ */
+static int	*ft_get_player(char ***map_ptr)
+{
+	int	row;
+	int	col;
+
+	row = -1;
+	while ((*map_ptr)[++row])
+	{
+		col = -1;
+		while ((*map_ptr)[row][++col])
+		{
+			if ((*map_ptr)[row][col] == 'N' || (*map_ptr)[row][col] == 'S'
+				|| (*map_ptr)[row][col] == 'W' || (*map_ptr)[row][col] == 'E')
+				return ((int[]){row, col});
+		}
+	}
+	return ((int[]){-1, -1});
 }
 
 void	flood_fill_util(char ***map_ptr, int x, int y, char replacement)
