777d91c6de81d3aa02a0efad6d2384b71ae7ac58
LeondavidZipp <leondavidzipp@gmx.de>
2024-07-03 10:38:57 +0200

    fixed segfaults, floodfill logic not correct yet (switch r and c)

--- src/parsing/validate_map.c
+++ src/parsing/validate_map.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 11:51:26 by lzipp             #+#    #+#             */
-/*   Updated: 2024/07/02 20:08:29 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/07/03 10:37:41 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 
 static bool	ft_flood_fill(char ***map_ptr, int x, int y);
 
+/**
+ * Copies the map for floodfill.
+ * @param map_ptr Map to copy.
+ * @param map_copy_ptr Pointer to the copied map.
+ * @param width Width of the map.
+ * @param height Height of the map.
+ * @return true if map was copied successfully, false otherwise.
+ */
 static bool	ft_copy_map(char ***map_ptr, char ***map_copy_ptr, int width,
 		int height)
 {
@@ -21,13 +29,13 @@ static bool	ft_copy_map(char ***map_ptr, char ***map_copy_ptr, int width,
 	int	j;
 	int	row_len;
 
-	*map_copy_ptr = ft_calloc(height, sizeof(char *));
+	(*map_copy_ptr) = ft_calloc(height + 1, sizeof(char *));
 	if (!*map_copy_ptr)
 		return (false);
 	i = -1;
 	while (++i < height)
 	{
-		(*map_copy_ptr)[i] = ft_calloc(width, sizeof(char));
+		(*map_copy_ptr)[i] = ft_calloc(width + 1, sizeof(char));
 		if (!(*map_copy_ptr)[i])
 			return (ft_free_2d_arr((void **)(*map_copy_ptr)), false);
 		row_len = ft_strlen((*map_ptr)[i]);
@@ -37,6 +45,9 @@ static bool	ft_copy_map(char ***map_ptr, char ***map_copy_ptr, int width,
 		while (j < width)
 			(*map_copy_ptr)[i][j++] = ' ';
 	}
+	i = -1;
+	while ((*map_ptr)[++i])
+		printf("--||%s||--\n", (*map_ptr)[i]);
 	return (true);
 }
 
@@ -58,11 +69,13 @@ bool	ft_validate_map(char ***map_ptr)
 	if (ft_copy_map(map_ptr, &map_copy, ft_map_width(map_ptr),
 			ft_map_height(map_ptr)) == false)
 		return (printf("Error: failed to copy map\n"), false);
-	i = -1;
-	while (map_copy[++i])
-		printf("||%s||\n", map_copy[i]);
+	i = 0;
+	while (map_copy[i])
+		printf("||%s||\n", map_copy[i++]);
+	printf("printed\n");
 	if (!map_copy)
 		return (printf("Error: failed to copy map\n"), false);
+	printf("Map copied\n");
 	if (ft_flood_fill(&map_copy, pos[0], pos[1]) == false)
 	{
 		ft_free_2d_arr((void **)map_copy);
