889e4c80b62f54488379893b387c1e4e5088af14
LeondavidZipp <leondavidzipp@gmx.de>
2024-07-03 21:47:28 +0200

    started changing static structs of player

--- inc/static.h
+++ inc/static.h
@@ -3,39 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   static.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmoritz <jmoritz@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 11:44:05 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/06/26 12:19:16 by jmoritz          ###   ########.fr       */
+/*   Updated: 2024/07/03 17:13:00 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STATIC_H
 #define STATIC_H
 
-typedef struct s_player {
-    t_vector_2d pos;
-    t_vector_2d dir;
-    t_vector_2d plane;
-    double move_speed;
-    double rot_speed;
-    double wall_dist;
-} t_player;
+# include "parsing.h"
 
-typedef struct s_map {
-    int **map;
-} t_map;
+# define MOVE_SPEED 0.1
+# define ROT_SPEED 0.05
+# define WALL_DIST 0.1
 
-typedef struct s_data  {
-    t_window *window;
-    t_player *player;
-    t_map *map;
-    mlx_texture_t **texture;
-} t_data;
+typedef struct	s_player {
+	t_vector_2d pos;
+	t_vector_2d dir;
+	t_vector_2d plane;
+	double move_speed;
+	double rot_speed;
+	double wall_dist;
+}				t_player;
 
-t_data      *init_data();
+typedef struct	s_map {
+	e_map_elements	**map;
+}				t_map;
+
+typedef struct	s_data  {
+	t_window *window;
+	t_player *player;
+	t_map *map;
+	mlx_texture_t **texture;
+}				t_data;
+
+t_data		*init_data();
 t_player	*init_player();
-t_map       *init_map();
-t_data      *static_data();
+t_map		*init_map();
+t_data		*static_data();
 
 #endif
--- src/init/init_player.c
+++ src/init/init_player.c
@@ -12,20 +12,64 @@
 
 #include "cub3d.h"
 
+static void			ft_get_pos_and_dir(t_vector_2d *pos, t_vector_2d *dir);
+static t_vector_2d	ft_get_dir(e_map_elements player);
+
 t_player	*init_player()
 {
-    t_player	*player;
+	t_player	*player;
 
-    player = malloc(sizeof(t_player));
-    if (player == NULL) {
-        printf("Error: Malloc failed\n");
-        exit(1);
-    }
-    player->pos = ft_vector_init(22, 12);
-    player->dir = ft_vector_init(-1, 0);
-    player->plane = ft_vector_init(0, 0.66);
-    player->move_speed = 0.1;
-    player->wall_dist = 0.1;
-    player->rot_speed = 0.05;
-    return (player);
+	player = malloc(sizeof(t_player));
+	if (player == NULL) {
+		printf("Error: Malloc failed\n");
+		exit(1);
+	}
+	ft_get_pos_and_dir(&(player->pos), &(player->dir));
+	player->plane = ft_vector_init(0, 0.66);
+	player->move_speed = MOVE_SPEED;
+	player->rot_speed = ROT_SPEED;
+	player->wall_dist = WALL_DIST;
+	return (player);
+}
+
+static void	ft_get_pos_and_dir(t_vector_2d *pos, t_vector_2d *dir)
+{
+	t_map	*map;
+	int		row;
+	int		col;
+
+	map = static_data()->map;
+	row = -1;
+	while (map->map[++row])
+	{
+		col = -1;
+		while (map->map[row][++col])
+		{
+			if (map->map[row][col] == PLAYER_EA
+				|| map->map[row][col] == PLAYER_NO
+				|| map->map[row][col] == PLAYER_SO
+				|| map->map[row][col] == PLAYER_WE)
+			{
+				*pos = ft_vector_init(col, row);
+				*dir = ft_get_dir(map->map[row][col]);
+			}
+		}
+	}
+}
+
+static t_vector_2d	ft_get_dir(e_map_elements player)
+{
+	t_vector_2d	dir;
+
+	if (player == PLAYER_EA)
+		dir = ft_vector_init(1, 0);
+	else if (player == PLAYER_NO)
+		dir = ft_vector_init(0, -1);
+	else if (player == PLAYER_SO)
+		dir = ft_vector_init(0, 1);
+	else if (player == PLAYER_WE)
+		dir = ft_vector_init(-1, 0);
+	else
+		dir = ft_vector_init(-1, -1);
+	return (dir);
 }
