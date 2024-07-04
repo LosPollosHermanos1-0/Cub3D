ee79406f3543c66eaceebf13b155167bc6e5bed2
LeondavidZipp <leondavidzipp@gmx.de>
2024-07-04 13:08:09 +0200

    updated init map

--- src/init/init_map.c
+++ src/init/init_map.c
@@ -6,21 +6,19 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 12:20:33 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/07/04 10:57:24 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/07/04 13:07:21 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "cub3d.h"
 
-t_map *init_map(char **content)
+t_map *init_map(e_map_elements ***map_ptr)
 {
-	t_map *map = malloc(sizeof(t_map));
-	if (map == NULL) {
-		printf("Error: Malloc failed\n");
-		exit(1);
-	}
-	map->map = NULL;
-	if (ft_get_map(content, &(map->map)) == false)
-		return (NULL);
+	t_map	*map;
+	
+	map = malloc(sizeof(t_map));
+	if (map == NULL)
+		return(printf("Error: map not allocted\n"), NULL);
+	map->map = *map_ptr;
 	return map;
 }
