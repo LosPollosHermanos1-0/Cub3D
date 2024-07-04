e9667c8e521cd5934b2fe067f8b4373d30394128
LeondavidZipp <leondavidzipp@gmx.de>
2024-07-04 13:17:23 +0200

    norminette fixes && return NULL

--- src/init/init_window.c
+++ src/init/init_window.c
@@ -3,31 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   init_window.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmoritz <jmoritz@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 19:12:47 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/06/20 19:14:01 by jmoritz          ###   ########.fr       */
+/*   Updated: 2024/07/04 13:17:06 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-t_window	*init_window()
+t_window	*init_window(void)
 {
-    t_window *window;
-    window = malloc(sizeof(t_window));
-    window->mlx = mlx_init(WINDOW_W_INIT, WINDOW_H_INIT, "Cub3D", true);
-    if (window->mlx == NULL) {
-        printf("Error: Malloc failed\n");
-        exit(1);
-    }
-    window->image = mlx_new_image(window->mlx, WINDOW_W_INIT, WINDOW_H_INIT);
-    if (window->image == NULL) {
-        printf("Error: Malloc failed\n");
-        exit(1);
-    }
-    window->width = WINDOW_W_INIT;
-    window->height = WINDOW_H_INIT;
+	t_window	*window;
 
-    return window;
+	window = malloc(sizeof(t_window));
+	window->mlx = mlx_init(WINDOW_W_INIT, WINDOW_H_INIT, "Cub3D", true);
+	if (window->mlx == NULL)
+		return (printf("Error: window mlx not allocated\n"), NULL);
+	window->image = mlx_new_image(window->mlx, WINDOW_W_INIT, WINDOW_H_INIT);
+	if (window->image == NULL)
+		return (printf("Error: window image not allocated\n"), NULL);
+	window->width = WINDOW_W_INIT;
+	window->height = WINDOW_H_INIT;
+	return (window);
 }
