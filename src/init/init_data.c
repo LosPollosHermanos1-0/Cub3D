0be3b995f87e005cf05aca9af1857d8429974f10
LeondavidZipp <leondavidzipp@gmx.de>
2024-07-04 17:28:47 +0200

    updated init data functionality

--- src/init/init_data.c
+++ src/init/init_data.c
@@ -6,20 +6,15 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 11:36:32 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/07/04 16:02:05 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/07/04 16:23:37 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "cub3d.h"
 
-static mlx_texture_t	**ft_load_texture(char ***textures);
-
 t_data	*init_data(void)
 {
 	t_data			*data;
-	char			**textures;
-	int				**rgb;
-	e_map_elements	**map;
 
 	data = malloc(sizeof(t_data));
 	if (data == NULL)
@@ -45,7 +40,7 @@ bool	ft_set_data(char **filepath, t_data **data_ptr)
 	// when not bonus
 	// (*data_ptr)->rgb = rgb;
 	(*data_ptr)->texture = init_texture(&textures);
-	ft_free_texctures(&textures);
+	ft_free_textures(&textures);
 	if ((*data_ptr)->texture == NULL)
 		return (false);
 	(*data_ptr)->window = init_window();
@@ -62,7 +57,7 @@ bool	ft_set_data(char **filepath, t_data **data_ptr)
 
 void	free_data(t_data **data)
 {
-	free_player((*data)->player);
-	free_map((*data)->map);
+	free_player(&((*data)->player));
+	free_map(&((*data)->map));
 	free(*data);
 }
