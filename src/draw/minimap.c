d67bd4178a1cb208532e55eb2cdc590548fde14c
LeondavidZipp <leondavidzipp@gmx.de>
2024-07-18 16:48:41 +0200

    added get color helper

--- src/draw/minimap.c
+++ src/draw/minimap.c
@@ -6,15 +6,16 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 12:08:21 by lzipp             #+#    #+#             */
-/*   Updated: 2024/07/18 15:33:36 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/07/18 16:48:20 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
 static void		make_mini_black(t_data *data);
-static void		draw_walls(t_data *data);
-static void		draw_player(t_data *data);
+static void		draw_mini_map_elements(t_data *data);
+static void		draw_mini_player(t_data *data);
+static uint32_t	get_color(t_data *data, int x, int y);
 
 /**
  * Repeatedly draws background of minimap
@@ -23,8 +24,8 @@ static void		draw_player(t_data *data);
 void	draw_mini_map(t_data *data)
 {
 	make_mini_black(data);
-	draw_walls(data);
-	draw_player(data);
+	draw_mini_map_elements(data);
+	draw_mini_player(data);
 }
 
 /**
@@ -46,20 +47,43 @@ static void make_mini_black(t_data *data)
 }
 
 /**
- * Draws walls on minimap
+ * Draws floor, walls, doors and other elements on minimap except player
  * @param data
- 
  */
-static void	draw_walls(t_data *data)
+static void	draw_mini_map_elements(t_data *data)
 {
-	;
+	(void)data;
 }
 
 /**
  * Draws player on minimap
  * @param data
  */
-static void	draw_player(t_data *data)
+static void	draw_mini_player(t_data *data)
 {
-	;
+	uint32_t	pos_y;
+	uint32_t	pos_x;
+	
+	pos_y = data->player.pos.y;
+	pos_x = data->player.pos.x;
+}
+
+static uint32_t	get_color(t_data *data, int x, int y)
+{
+	// everything outside of map
+	if ((x < 0 || y < 0 || x >= data->map->width
+			|| y >= data->map->height)
+		|| data->map->map[y][x] == END
+		|| data->map->map[y][x] == OUTSIDE)
+		return (0x000000FF);
+	// walls
+	else if (data->map->map[y][x] == WALL)
+		return (0x0000FFFF);
+	// doors
+	else if (data->map->map[y][x] == DOOR)
+		return (0xFFFFFFFF);
+	// else if (data->map->map[y][x] == OPEN_DOOR)
+	// 	return (0xAA00AAFF);
+	// floor
+	return (0xFF0000FF);
 }
