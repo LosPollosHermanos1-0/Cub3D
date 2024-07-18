f83d07a0f3a6d283192fb0a38b7ceb8c6d7a9dd8
LeondavidZipp <leondavidzipp@gmx.de>
2024-07-18 15:33:55 +0200

    added template static functions

--- src/draw/minimap.c
+++ src/draw/minimap.c
@@ -6,13 +6,15 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 12:08:21 by lzipp             #+#    #+#             */
-/*   Updated: 2024/07/18 15:11:28 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/07/18 15:33:36 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void make_mini_black(t_data *data);
+static void		make_mini_black(t_data *data);
+static void		draw_walls(t_data *data);
+static void		draw_player(t_data *data);
 
 /**
  * Repeatedly draws background of minimap
@@ -21,6 +23,8 @@ static void make_mini_black(t_data *data);
 void	draw_mini_map(t_data *data)
 {
 	make_mini_black(data);
+	draw_walls(data);
+	draw_player(data);
 }
 
 /**
@@ -41,7 +45,21 @@ static void make_mini_black(t_data *data)
 	}
 }
 
-// void	draw_lines(t_data *data)
-// {
-// 	;
-// }
+/**
+ * Draws walls on minimap
+ * @param data
+ 
+ */
+static void	draw_walls(t_data *data)
+{
+	;
+}
+
+/**
+ * Draws player on minimap
+ * @param data
+ */
+static void	draw_player(t_data *data)
+{
+	;
+}
