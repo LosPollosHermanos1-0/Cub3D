a6a03002aab428e30fdcb16a510aa3c476f0dee7
LeondavidZipp <leondavidzipp@gmx.de>
2024-07-19 15:42:22 +0200

    added fov, segfaulting still

--- src/draw/minimap.c
+++ src/draw/minimap.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 12:08:21 by lzipp             #+#    #+#             */
-/*   Updated: 2024/07/19 15:08:25 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/07/19 15:41:33 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ static void		draw_mini_map_element(t_data **data, int map_x, int map_y,
 					double scale);
 static void		draw_mini_player(t_data **data, double scale);
 static uint32_t	get_color(t_data *data, int x, int y);
+static void		draw_fov(t_data **data);
 
 /**
  * Repeatedly draws minimap
@@ -81,56 +82,79 @@ static void	draw_mini_map_element(t_data **data, int map_x, int map_y,
  * Draws player on minimap
  * @param data
  */
-// static void	draw_mini_player(t_data **data, double scale)
-// {
-// 	uint32_t	pos_y;
-// 	uint32_t	pos_x;
-// 	int			pixel_y;
-// 	int			pixel_x;
-
-// 	pos_y = (*data)->player.pos.y;
-// 	pos_x = (*data)->player.pos.x;
-// 	pixel_y = -1;
-// 	while (++pixel_y < scale)
-// 	{
-// 		pixel_x = -1;
-// 		while (++pixel_x < scale)
-// 		{
-// 			if (pos_y * scale + pixel_x >= 0
-// 				&& pos_y * scale + pixel_x < (*data)->map->width * scale
-// 				&& pos_x * scale + pixel_y >= 0
-// 				&& pos_x * scale + pixel_y < (*data)->map->height * scale)
-// 				mlx_put_pixel((*data)->window->mini_image,
-// 					pos_y * scale + pixel_y, pos_x * scale + pixel_x, 0xAAFFFFFF);
-// 		}
-// 	}
-// }
-
 static void	draw_mini_player(t_data **data, double scale)
 {
+	uint32_t	pos_y;
+	uint32_t	pos_x;
 	int			pixel_y;
 	int			pixel_x;
 
-	(*data)->player.pos.y = (*data)->player.pos.y;
-	(*data)->player.pos.x = (*data)->player.pos.x;
-	double radius = scale / 2;
-	double centerX = (*data)->player.pos.x * scale + radius; // Center of the circle in the scaled grid
-	double centerY = (*data)->player.pos.y * scale + radius; // Center of the circle in the scaled grid
+	pos_y = (*data)->player.pos.y;
+	pos_x = (*data)->player.pos.x;
 	pixel_y = -1;
 	while (++pixel_y < scale)
 	{
 		pixel_x = -1;
 		while (++pixel_x < scale)
 		{
-			double distanceX = (centerX - ((*data)->player.pos.x * scale + pixel_x));
-			double distanceY = (centerY - ((*data)->player.pos.y * scale + pixel_y));
-			if ((distanceX * distanceX + distanceY * distanceY) <= (radius * radius))
-			{
+			if (pos_y * scale + pixel_x >= 0
+				&& pos_y * scale + pixel_x < (*data)->map->width * scale
+				&& pos_x * scale + pixel_y >= 0
+				&& pos_x * scale + pixel_y < (*data)->map->height * scale)
 				mlx_put_pixel((*data)->window->mini_image,
-							(*data)->player.pos.y * scale + pixel_y, (*data)->player.pos.x * scale + pixel_x, 0xAAFFFFFF);
-			}
+					pos_y * scale + pixel_y, pos_x * scale + pixel_x, 0xAAFFFFFF);
 		}
 	}
+	draw_fov(data);
+}
+
+// static void	draw_mini_player(t_data **data, double scale)
+// {
+// 	int			pixel_y;
+// 	int			pixel_x;
+
+// 	(*data)->player.pos.y = (*data)->player.pos.y;
+// 	(*data)->player.pos.x = (*data)->player.pos.x;
+// 	double radius = scale / 2;
+// 	double centerX = (*data)->player.pos.x * scale + radius; // Center of the circle in the scaled grid
+// 	double centerY = (*data)->player.pos.y * scale + radius; // Center of the circle in the scaled grid
+// 	pixel_y = -1;
+// 	while (++pixel_y < scale + radius)
+// 	{
+// 		pixel_x = -1;
+// 		while (++pixel_x < scale + radius)
+// 		{
+// 			double distanceX = (centerX - ((*data)->player.pos.x * scale + pixel_x));
+// 			double distanceY = (centerY - ((*data)->player.pos.y * scale + pixel_y));
+// 			if ((distanceX * distanceX + distanceY * distanceY) <= (radius * radius))
+// 			{
+// 				mlx_put_pixel((*data)->window->mini_image,
+// 							(*data)->player.pos.y * scale + pixel_y,
+// 							(*data)->player.pos.x * scale + pixel_x, 0xAAFFFFFF);
+// 			}
+// 		}
+// 	}
+// 	// draw_fov(data);
+// }
+
+static void	draw_fov(t_data **data)
+{
+	double		angle;
+	t_player	player;
+	t_vector_2d	start;
+	t_vector_2d	left_end;
+	t_vector_2d	right_end;
+
+	player = (*data)->player;
+	start = player.pos;
+	angle = atan2f(player.dir.y, player.dir.x) - (double)(FOV / 2);
+	left_end.x = start.x + cosf(angle) * 10;
+	left_end.y = start.y + sinf(angle) * 10;
+	angle = atan2f(player.dir.y, player.dir.x) + (double)(FOV / 2);
+	right_end.x = start.x + cosf(angle) * 10;
+	right_end.y = start.y + sinf(angle) * 10;
+	draw_line((*data)->minimap->minimap, start, left_end, 0xFFFFFFFF);
+	draw_line((*data)->minimap->minimap, start, right_end, 0xFFFFFFFF);
 }
 
 // double radius = scale / 2;
