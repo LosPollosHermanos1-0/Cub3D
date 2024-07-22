10384989256df02c99b766b3d6420c08ca8c8855
LeondavidZipp <leondavidzipp@gmx.de>
2024-07-22 21:06:07 +0200

    norm fixed get_pixel_from_image

--- src/draw/get_pixel_from_image.c
+++ src/draw/get_pixel_from_image.c
@@ -3,24 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   get_pixel_from_image.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmoritz <jmoritz@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 23:32:39 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/07/02 23:33:05 by jmoritz          ###   ########.fr       */
+/*   Updated: 2024/07/22 21:05:40 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-inline uint32_t get_pixel(const mlx_texture_t *texture, uint32_t x, uint32_t y) {
+inline uint32_t	get_pixel(const mlx_texture_t *texture, uint32_t x, uint32_t y)
+{
+	uint32_t	index;
+	uint8_t		*pixel_address;
+
 	if (x >= texture->width || y >= texture->height)
 		return (0);
-
-	const uint32_t index = (y * texture->width + x) * texture->bytes_per_pixel;
-	const uint8_t *pixel_address = texture->pixels + index;
-
+	index = (y * texture->width + x) * texture->bytes_per_pixel;
+	pixel_address = texture->pixels + index;
 	return (((uint32_t)pixel_address[0] << 24)
-			| ((uint32_t)pixel_address[1] << 16)
-			| ((uint32_t)pixel_address[2] << 8)
-			| (uint32_t)pixel_address[3]);
+		| ((uint32_t)pixel_address[1] << 16)
+		| ((uint32_t)pixel_address[2] << 8)
+		| (uint32_t)pixel_address[3]);
 }
