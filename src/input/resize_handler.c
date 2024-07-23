f7be627b93a31de2731c55318d91b49d6bd17f5a
LeondavidZipp <leondavidzipp@gmx.de>
2024-07-23 09:11:03 +0200

    norm fixed mouse && resize handler

--- src/input/resize_handler.c
+++ src/input/resize_handler.c
@@ -6,23 +6,26 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 13:24:54 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/07/18 15:31:07 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/07/23 09:10:09 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	resize_handler(int32_t width, int32_t height, void* d)
+void	resize_handler(int32_t width, int32_t height, void *d)
 {
-	t_data *data = d;
-	t_window *window = data->window;
+	t_data		*data;
+	t_window	*window;
+
+	data = d;
+	window = data->window;
 	mlx_delete_image(window->mlx, window->image);
 	window->width = width;
 	window->height = height;
 	window->image = mlx_new_image(window->mlx, window->width,
-		window->height);
+			window->height);
 	data->z_buffer = ft_calloc(sizeof(int) * data->window->width,
-		sizeof(int));
+			sizeof(int));
 	ft_memset(data->z_buffer, -1, sizeof(int) * data->window->width);
 	mlx_image_to_window(window->mlx, window->image, 0, 0);
 	mlx_image_to_window(window->mlx, window->mini_image, 0, 0);
