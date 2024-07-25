31cd9cf0cf892d2c43f398e184f53e22fbd91da2
LeondavidZipp <leondavidzipp@gmx.de>
2024-07-25 16:48:58 +0200

    norm fixed draw line

--- src/draw/draw_line.c
+++ src/draw/draw_line.c
@@ -6,11 +6,13 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 14:20:06 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/07/25 16:22:11 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/07/25 16:48:39 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static void	do_work2(t_draw_line_data *line_data, t_vector_2d *start);
 
 void	do_work(t_vector_2d start, t_vector_2d end, t_vector_2d *sign)
 {
@@ -42,40 +44,79 @@ void	clamp_to_window(t_vector_2d *point)
 		point->y = data->window->height - 1;
 }
 
+// void	draw_line(mlx_image_t *img, t_vector_2d start, t_vector_2d end,
+// 		uint32_t color)
+// {
+// 	t_data		*data;
+// 	t_vector_2d	delta;
+// 	t_vector_2d	sign;
+// 	t_vector_2d	cur;
+// 	int			error;
+// 	int			error2;
+
+// 	data = static_data();
+// 	clamp_to_window(&start);
+// 	clamp_to_window(&end);
+// 	delta = ft_vector_init(fabs(end.x - start.x), fabs(end.y - start.y));
+// 	do_work(start, end, &sign);
+// 	error = (int)(delta.x - delta.y);
+// 	cur = start;
+// 	while (1)
+// 	{
+// 		if (cur.x >= 0 && cur.x < data->window->width && cur.y >= 0
+// 			&& cur.y < data->window->height)
+// 			mlx_put_pixel(img, (int)cur.x, (int)cur.y, color);
+// 		if ((int)cur.x == (int)end.x && (int)cur.y == (int)end.y)
+// 			break ;
+// 		error2 = error * 2;
+// 		if (error2 > -(int)delta.y)
+// 		{
+// 			error -= (int)delta.y;
+// 			cur.x += sign.x;
+// 		}
+// 		if (error2 < (int)delta.x)
+// 		{
+// 			error += (int)delta.x;
+// 			cur.y += sign.y;
+// 		}
+// 	}
+// }
+
 void	draw_line(mlx_image_t *img, t_vector_2d start, t_vector_2d end,
 		uint32_t color)
 {
-	t_data		*data;
-	t_vector_2d	delta;
-	t_vector_2d	sign;
-	t_vector_2d	cur;
-	int			error;
-	int			error2;
+	t_draw_line_data	line_data;
+	t_data				*data;
 
 	data = static_data();
 	clamp_to_window(&start);
 	clamp_to_window(&end);
-	delta = ft_vector_init(fabs(end.x - start.x), fabs(end.y - start.y));
-	do_work(start, end, &sign);
-	error = (int)(delta.x - delta.y);
-	cur = start;
+	line_data.delta = ft_vector_init(fabs(end.x - start.x),
+			fabs(end.y - start.y));
+	do_work(start, end, &(line_data.sign));
+	line_data.error = (int)(line_data.delta.x - line_data.delta.y);
 	while (1)
 	{
-		if (cur.x >= 0 && cur.x < data->window->width && cur.y >= 0
-			&& cur.y < data->window->height)
-			mlx_put_pixel(img, (int)cur.x, (int)cur.y, color);
-		if ((int)cur.x == (int)end.x && (int)cur.y == (int)end.y)
+		if (start.x >= 0 && start.x < data->window->width && start.y >= 0
+			&& start.y < data->window->height)
+			mlx_put_pixel(img, (int)start.x, (int)start.y, color);
+		if ((int)start.x == (int)end.x && (int)start.y == (int)end.y)
 			break ;
-		error2 = error * 2;
-		if (error2 > -(int)delta.y)
-		{
-			error -= (int)delta.y;
-			cur.x += sign.x;
-		}
-		if (error2 < (int)delta.x)
-		{
-			error += (int)delta.x;
-			cur.y += sign.y;
-		}
+		do_work2(&line_data, &start);
+	}
+}
+
+static void	adjust_values(t_draw_line_data *line_data, t_vector_2d *start)
+{
+	line_data->error2 = line_data->error * 2;
+	if (line_data->error2 > -(int)line_data->delta.y)
+	{
+		line_data->error -= (int)line_data->delta.y;
+		start->x += line_data->sign.x;
+	}
+	if (line_data->error2 < (int)line_data->delta.x)
+	{
+		line_data->error += (int)line_data->delta.x;
+		start->y += line_data->sign.y;
 	}
 }
