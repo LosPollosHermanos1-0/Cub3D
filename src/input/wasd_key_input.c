ffe6c244e45b96e0c2bd0c4bd611da013a40ea10
LeondavidZipp <leondavidzipp@gmx.de>
2024-07-25 14:48:13 +0200

    completely fixed norm

--- src/input/wasd_key_input.c
+++ src/input/wasd_key_input.c
@@ -3,29 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   wasd_key_input.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmoritz <jmoritz@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 08:58:01 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/07/02 08:58:07 by jmoritz          ###   ########.fr       */
+/*   Updated: 2024/07/25 14:47:42 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void make_window_black() {
-	t_data *data = static_data();
-	for (int y = 0; y < data->window->height; ++y) {
-		for (int x = 0; x < data->window->width; ++x) {
-			mlx_put_pixel(data->window->image,  x, y, 0x00000000);
+void	make_window_black(void)
+{
+	t_data	*data;
+	int		x;
+	int		y;
+
+	data = static_data();
+	y = -1;
+	while (++y < data->window->height)
+	{
+		x = -1;
+		while (x < data->window->width)
+		{
+			mlx_put_pixel(data->window->image, x, y, 0x00000000);
 		}
 	}
 }
 
 void	wasd_key_input(void *data)
 {
-	t_data	*d = data;
-	mlx_t *mlx = d->window->mlx;
+	t_data		*d;
+	mlx_t		*mlx;
+	int			i;
 
+	d = (t_data *)data;
+	mlx = d->window->mlx;
 	if (mlx_is_key_down(mlx, MLX_KEY_W) || mlx_is_key_down(mlx, MLX_KEY_A)
 		|| mlx_is_key_down(mlx, MLX_KEY_S) || mlx_is_key_down(mlx, MLX_KEY_D))
 		move_player(d);
@@ -33,15 +45,14 @@ void	wasd_key_input(void *data)
 		rotate_player(d, d->player.rot_speed);
 	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
 		rotate_player(d, -d->player.rot_speed);
-
 	draw_rays();
-
 	sort_sprites(d);
-	for (int i = 0; i < d->opponent_count; ++i) {
+	i = -1;
+	while (++i < d->opponent_count)
+	{
 		draw_sprite(data, &d->sprites[i]);
 		move_opponent(d, &d->sprites[i]);
 	}
 	draw_mini_map(d);
 	draw_door_icon(d);
 }
-
