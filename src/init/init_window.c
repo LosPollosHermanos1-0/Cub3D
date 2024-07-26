c50faef10961bd4bbccb2b520347cf5d46e807bc
LeondavidZipp <leondavidzipp@gmx.de>
2024-07-26 17:26:33 +0200

    norm && leak fixed

--- src/init/init_window.c
+++ src/init/init_window.c
@@ -6,11 +6,13 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 19:12:47 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/07/25 17:04:01 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/07/26 17:26:15 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static void	fill_window_values(t_window *window);
 
 void	add_images_to_window(t_window *window)
 {
@@ -28,7 +30,9 @@ void	add_images_to_window(t_window *window)
 
 t_window	*init_window(void)
 {
-	t_window	*window;
+	t_window		*window;
+	mlx_texture_t	*closed;
+	mlx_texture_t	*open;
 
 	window = malloc(sizeof(t_window));
 	window->mlx = mlx_init(WINDOW_W_INIT, WINDOW_H_INIT, "Cub3D", true);
@@ -39,18 +43,25 @@ t_window	*init_window(void)
 			MINI_IMAGE_W, MINI_IMAGE_H);
 	if (window->image == NULL || window->mini_image == NULL)
 		return (printf("Error: window image not allocated\n"), NULL);
-	window->door_icon_open = mlx_texture_to_image(
-			window->mlx, mlx_load_png("textures/door-open-icon.png"));
-	window->door_icon_close = mlx_texture_to_image(
-			window->mlx, mlx_load_png("textures/door-close-icon.png"));
+	closed = mlx_load_png("textures/door-close-icon.png");
+	window->door_icon_open = mlx_texture_to_image(window->mlx, closed);
+	open = mlx_load_png("textures/door-open-icon.png");
+	window->door_icon_close = mlx_texture_to_image(window->mlx, open);
+	free(closed);
+	free(open);
 	if (window->door_icon_open == NULL || window->door_icon_close == NULL)
 		return (printf("Error: door icon not allocated\n"), NULL);
+	fill_window_values(window);
+	add_images_to_window(window);
+	return (window);
+}
+
+static void	fill_window_values(t_window *window)
+{
 	window->width = WINDOW_W_INIT;
 	window->height = WINDOW_H_INIT;
 	window->mini_width = MINI_IMAGE_W;
 	window->mini_height = MINI_IMAGE_H;
 	window->door_icon_width = DOOR_ICON_W;
 	window->door_icon_height = DOOR_ICON_H;
-	add_images_to_window(window);
-	return (window);
 }
